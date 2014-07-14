ae132264f48134542dca336966c94351f2a3cdec
bingo <blackbing@gmail.com>
2014-07-15 02:20:11 +0800

    Polyfill for Function.prototype.bind https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind

--- test/polyfill/phantom_bind_polyfill.js
+++ test/polyfill/phantom_bind_polyfill.js
@@ -1,42 +1,24 @@
-/////////////////////////////////////
-// Taken from ariya/phantomjs#10522
-//
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+      // closest thing possible to the ECMAScript 5
+      // internal IsCallable function
+      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+    }
 
-'use strict';
+    var aArgs = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP = function () {},
+        fBound = function () {
+          return fToBind.apply(this instanceof fNOP && oThis
+                 ? this
+                 : oThis,
+                 aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
 
-var isFunction = function(o) {
-  return typeof o === 'function';
-};
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
 
-
-var bind,
-  slice = [].slice,
-  proto = Function.prototype,
-  featureMap;
-
-featureMap = {
-  'function-bind': 'bind'
-};
-
-function has(feature) {
-  var prop = featureMap[feature];
-  return isFunction(proto[prop]);
-}
-
-// check for missing features
-if (!has('function-bind')) {
-  // adapted from Mozilla Developer Network example at
-  // https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/bind
-  bind = function bind(obj) {
-    var args = slice.call(arguments, 1),
-      self = this,
-      Nop = function() { },
-      bound = function() {
-        return self.apply(this instanceof Nop ? this : (obj || {}), args.concat(slice.call(arguments)));
-      };
-    Nop.prototype = this.prototype || {}; // Firefox cries sometimes if prototype is undefined
-    bound.prototype = new Nop();
-    return bound;
+    return fBound;
   };
-  proto.bind = bind;
 }
